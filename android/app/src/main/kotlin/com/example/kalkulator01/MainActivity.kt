218e26e4c56a45f38cac9ae5b5d3333b5e42ddb3
Moch Fajrul Falah <mfajrulfalah.ff@gmail.com>
2022-11-04 14:16:22 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/kalkulator01/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.kalkulator01
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,7 @@
+// import 'package:flutter/material.dart';
+
+// final Color mainHexColor = const Color(0x1C1C1C);
+// final Color accentHexColor = const Color(0xFCC91C);
+
+
+
